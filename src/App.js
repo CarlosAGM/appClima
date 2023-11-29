da1444a4e1220e3bef524979dc113621e3eef303
Carlos Aguero Marquizani <carlos.aguero.marquizani@gmail.com>
2023-11-29 11:46:14 -0300

    app clima

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [values, setValues] = useState('')
+  const [icons, setIcon] = useState('')
+
+  const URL =  `https://api.openweathermap.org/data/2.5/weather?q=valdivia&lang=es&units=metric&appid=74250c658f566ee0d95b9ffa048866c9`
+
+  const getData = async () => {
+    await fetch(URL)
+    .then(response => {return response.json()})
+    .then( data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Aplicación de Clima en React</h2>
     </div>
   );
 }
---
+++ src/components/icons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const icons = () => {
+  return (
+    <div>icons</div>
+  )
+}
+
+export default icons
