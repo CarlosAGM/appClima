6dd7b466d6ad5de6fb2a3c32ccfe63ddd71f9e3b
Carlos Aguero Marquizani <carlos.aguero.marquizani@gmail.com>
2023-12-04 16:29:50 -0300

    app clima avance

--- src/App.js
+++ src/App.js
@@ -1,34 +1,85 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import icons from './components/icons.js';
 
 function App() {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('Viña del Mar')
   const [values, setValues] = useState('')
-  const [icons, setIcon] = useState('')
+  const [icon, setIcon] = useState('')
 
-  const URL =  `https://api.openweathermap.org/data/2.5/weather?q=valdivia&lang=es&units=metric&appid=74250c658f566ee0d95b9ffa048866c9`
+  const URL =  `https://api.openweathermap.org/data/2.5/weather?q=${search}&lang=es&units=metric&appid=${process.env.REACT_APP_API_KEY}`
 
   const getData = async () => {
     await fetch(URL)
     .then(response => {return response.json()})
     .then( data => {
-      console.log(data)
+      if(data.cod >= 400) {
+        setValues(false)
+      } else {
+        console.log(data)
+        console.log(data.weather[0].main)
+        setIcon(data.weather[0].main)
+        setValues(data)
+      }
+      //console.log(data)
     })
     .catch(error => {
       console.log(error)
     })
   }
 
+  const handleSearch = (e) => {
+    if(e.key === 'Enter') {
+      //console.log(e.target.value)
+      setSearch(e.target.value)
+    }
+  }
+
   useEffect(()=>{
     getData()
-  },[])
+  },[search]) 
 
 
   return (
-    <div className="App">
+    <>
+    <div className="contenedor">
       <h2>Aplicación de Clima en React</h2>
+      <div className='fila'>
+
+      <input
+        onKeyDown={handleSearch}
+        type='text'
+        autoFocus
+      />
+
+      </div>
     </div>
+    <div className='card'>
+      {(values) ? (
+        <div className='card-contenedor'>
+          <h1 className='nombre-ciudad'>
+            {values.name}
+          </h1>
+          <p className='temperatura'>
+            {values.main.temp.toFixed(0)}&deg;
+          </p>
+          <img className='icon' src={icons(icon)} alt="icon-weather" />
+          <div className='card-pie'>
+            <p className='temp-max-min'>
+            {values.main.temp_min.toFixed(0)}&deg;  |  {values.main.temp_max.toFixed(0)}&deg;
+            </p>
+          </div>
+
+        </div>
+      ) : (
+        <h1>{"Ciudad no encontrada"}</h1>
+      )}
+
+    </div>
+
+
+    </>
   );
 }
 
--- src/components/icons.js
+++ src/components/icons.js
@@ -1,9 +1,54 @@
 import React from 'react'
 
-export const icons = () => {
-  return (
-    <div>icons</div>
-  )
+const icons = (icon) => {
+  switch (icon) {
+    case 'Thunderstorm':
+      icon='icons/thunderstorms-rain.svg'
+      console.log('tormenta')
+      break;
+
+    case 'Clear':
+      icon='icons/clear-day.svg'
+      console.log('Despejado')
+      break;
+    
+    case 'Clouds':
+      icon='icons/fog.svg'
+      console.log('Nubes')
+      break;
+    case 'Fog':
+      icon='icons/fog.svg'
+      console.log('Nubes')
+      break;
+    case 'Haze':
+      icon='icons/haze.svg'
+      console.log('Bruma')
+      break;
+    case 'Smoke':
+      icon='icons/smoke.svg'
+      console.log('Humo')
+      break;
+    case 'Rain':
+      icon='icons/rain.svg'
+      console.log('Lluvia')
+      break;
+    case 'Snow':
+      icon='icons/snow.svg'
+      console.log('Nieve')
+      break;
+    case 'Drizzle':
+      icon='icons/drizzle.svg'
+      console.log('Llovizna')
+      break;
+
+
+      
+
+    default:
+      icon='icons/clear-day.svg'
+      console.log('Despejado')
+  }
+  return icon
 }
 
 export default icons
